e99809b91afe33b05cb02e43a6f00277497150b6
MANI199-CA <camachoalejandro96@gmail.com>
2026-07-04 23:13:24 -0600

    destructuracion y spreading

--- CURSO MOUREDEV JS/destructuring-spreading.js
+++ CURSO MOUREDEV JS/destructuring-spreading.js
@@ -22,6 +22,39 @@ let myObject = {
     puertas: 2
 }
 
+// en destructuracion de objetos el nombre de las variables debe ser igual al nombre de la propiedad del objeto
 let {vehiculo, color, año, var2}= myObject;
 console.log(vehiculo)
 console.log(var2)
+
+
+/*spreading (...) expandir los valores de un objeto o array sacarlos de la caja y ponerlos en otra
+pienso que es como usar un forEach y un push */ 
+
+let myArray2 = [...myArray];
+
+console.log(myArray2);
+
+/* aparte de usar el operador spread para guardar los valores de un array dentro de otro tambien podemos agregar valores nuevos en la 
+inicializacion del nuevo array como en este caso que agregamos el 5 y el 23 ademas de los valores del otro array*/
+
+
+let array3 =[...myArray2, 5, 23];
+
+console.log(array3)
+
+// tambien podemos combinar varios array o concatenarlos
+
+let arrayMasivo =[...myArray,...array3,...myArray2];
+console.log(arrayMasivo)
+
+/*SPREADING DE OBJETOS muy similar a la de los arrays pero aqui se usa {} */
+
+let newObject= {...myObject};
+console.log(newObject)
+
+/*en este ejemplo no solo creamos un nuevo objetos usando ... con los valores de otro objeto, sino que tambien agregamos un
+nueva propiedad*/
+let objectMas= {...myObject, motor: 'estandar'};
+
+console.log(objectMas)
